c1a5d41e468d2f00a20aa52360f3526d9aa736f6
affq <adrianfabisiewicz@gmail.com>
2024-03-12 10:57:08 +0100

    edit tests

--- download_main.py
+++ download_main.py
@@ -1,18 +1,45 @@
 import requests
 from owslib.wfs import WebFeatureService
 
-def download_file(url):
-    url = "https://opendata.geoportal.gov.pl/bdot10k/schemat2021/SHP/22/2209_SHP.zip"
+def download_and_save_file(input, output):
+    response = requests.get(input)
 
-    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f"Nie udało się pobrać pliku z {input}. Kod błędu: {response.status_code}.")
 
-    with open("pobrany.zip", "wb") as file:
+    with open(output, "wb") as file:
         file.write(response.content)
+ 
+def wfs_get_service_name(url, version="1.0.0"):
+    try:
+        wfs = WebFeatureService(url=url, version=version)
+        return wfs.identification.title
+    except Exception as e:
+        raise Exception(f"Nie udało się pobrać nazwy serwisu. {e}")
 
 if __name__ == '__main__':
-    wfs_service_url = 'https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WFS/SkorowidzPrawdziwejOrtofotomapy'
-    wfs11 = WebFeatureService(url=wfs_service_url)
+    # Testy pobierania
+    download_and_save_file("https://bi.im-g.pl/im/e3/12/14/z21048035AMP.jpg", "doggo.jpg")
+    download_and_save_file("https://opendata.geoportal.gov.pl/NumDaneWys/NMT/77218/77218_1301111_N-34-139-C-b-1-4.asc", "nmt.asc")
+
+    try:
+        download_and_save_file("xdxdxdxdxd", "xd")
+    except Exception as e:
+        print("Pobieranie z błędnego URL się nie udało. Sukces!")
+        print("Wyjątek:", e)
+    else:
+        print("Ups...")
+
+    # Testy WFS
+    print("NMT", wfs_get_service_name("https://mapy.geoportal.gov.pl/wss/service/PZGIK/NumerycznyModelTerenuEVRF2007/WFS/Skorowidze"))
+    print("NMPT", wfs_get_service_name("https://mapy.geoportal.gov.pl/wss/service/PZGIK/NumerycznyModelPokryciaTerenuEVRF2007/WFS/Skorowidze"))
+    print("BDOT 10k", wfs_get_service_name("https://mapy.geoportal.gov.pl/wss/service/PZGIK/BDOT10k/WFS/StatystykiSieciWodnej", "2.0.0"))
     
-    response = wfs11.getfeature(
-        bbox=(521726, 672247,5500500)
-        )
+    try:
+        wfs_get_service_name("bledny_adres")
+    except Exception as e:
+        print("Zapytanie do błędnego serwisu nie przeszło. Sukces!")
+        print("Wyjątek:", e)
+    else:
+        print("Ups...")
+    
